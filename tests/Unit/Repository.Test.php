f666cbe00ab171702f0ab044a8ab8044dac1e867
Андрей Инюцин <andrey.inyutsin@elocont.ru>
2025-11-06 16:49:00 +0400

    fixing

--- tests/Unit/Repository.Test.php
+++ tests/Unit/Repository.Test.php
@@ -15,5 +15,23 @@ class Repository extends TestCase
         $this->assertContainsOnlyInstancesOf(HyppoEngine\Repository::class, [$repository]);
     }
 
+    public function test_path()
+    {
+        // Создаём репозиторий типа файл
+        $repository = HyppoEngine\Repository::create();
+        $repository->path(Document::PATH);
+        $this->assertEquals($repository->path(), Document::PATH);
+    }
+
+    public function test_getByPrimaryKey()
+    {
+        // Создаём репозиторий типа файл
+        $repository = HyppoEngine\Repository::create();
+        $repositoryNode = $repository->path(Document::PATH)->getByPrimaryKey("/README.md");
+        // Убеждаемся что получили объект нужного класса
+        $this->assertContainsOnlyInstancesOf(HyppoEngine\DTO\RepositoryNode::class, [$repositoryNode]);
+
+    }
+
 }
 
